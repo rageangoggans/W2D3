fb6a3d4f6c3b69ed07253b29c122aac0f1fc8539
Your Name <you@example.com>
2015-09-27 18:58:14 -0400

    3

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Answer");
+            Console.ReadLine();
+        
+            int x = 5;
+           
+            Console.WriteLine((20 % 5 == 7 && 20 % 7 == 0)? ("yes:no"));
+
+
+
+        }
+    }
+}
